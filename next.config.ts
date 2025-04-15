a7fb50df932cf8b35eb0a63c751a04a68ffab6a2
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2025-04-16 01:59:38 +0600

    initializing the project with image next configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*', // This allows images from all domains with HTTPS
+      },
+      {
+        protocol: 'http',
+        hostname: '*', // This allows images from all domains with HTTP
+      },
+    ],
+    minimumCacheTTL: 60,
+  },
 };
 
 export default nextConfig;
